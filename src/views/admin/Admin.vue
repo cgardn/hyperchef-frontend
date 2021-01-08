bbb329b67603e43da7f798ca980a52a43ed50d2f
cgardn <christopher.gardn@gmail.com>
2021-01-08 13:35:50 -0500

    remove obsolete/unused code

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -14,72 +14,16 @@
 
     <div v-else>
       <!-- after login -->
-      <div class="card">
-        <button @click="downloadData" class="btn btn-info">Get Data</button>
+      <div class="card px-3 py-3">
         <div class="row">
-          <div class="col" v-for="tab in components" :key="tab">
+          <div class="col text-center" v-for="tab in components" :key="tab">
             <button @click="currentComponent = tab">{{tab}}</button>
           </div>
-          <!--
-          <div class="col">
-            <button @click="curren">New Recipe</button>
-          </div>
-          <div class="col">
-            <button @click="allIngredients">Ingredients</button>
-          </div>
-          <div class="col">
-            <button @click="allIngredientTags">Ingredient Tags</button>
-          </div>
-          <div class="col">
-            <button @click="allEquipment">Equipment</button>
-          </div>
-          -->
         </div>
-      </div>
-      <div v-if="isLoading">
-        <span class="text-light">Waiting for data from server</span>
       </div>
 
       <!-- tab container -->
       <component :is="currentComponent"></component>
-
-      <!-- ingredient list -->
-      <div class="card" v-if="currentComponent=='ingredientList'">
-        <div class="row" v-for="(ing, idx) in Object.keys(currentData)" :key="idx">
-          <div class="col">
-            <IngredientForm :id="ing" :data="currentData[ing]"></IngredientForm>
-          </div>
-        </div>
-      </div>
-
-      <!-- ingredient tags -->
-      <div class="card" v-if="currentComponent=='ingredientTagList'">
-        <div v-if="adminLoading">
-          <span>Submitting changes...</span>
-          <span>{{this.adminMsg}}</span>
-        </div>
-        <div class="row"
-          v-for="(iTag, idx) in Object.keys(currentData)"
-          :key="idx">
-          <div class="col">
-            <span>ID: {{iTag}} </span>
-            <span>Name: </span>
-            <input type="text" v-model="currentData[iTag]['name']">
-            <button @click="updateIngredientTag(iTag, currentData[iTag]['name'])">
-              Submit
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <!-- equipment -->
-      <div class="card" v-if="currentComponent=='equipmentList'">
-        <div class="row" v-for="(eq, idx) in currentData" :key="idx">
-          <div class="col">
-            {{eq}}
-          </div>
-        </div>
-      </div>
 
     </div>
 
@@ -114,107 +58,14 @@ export default {
       currentComponent: "",
       username: "",
       password: "",
-      adminLoading: false,
-      adminMsg: "",
-      isLoading: false,
       isAuth: false,
-      currentData: {},
-      recipes: {},
-      recipe_types: [],
-      ingredients: [],
-      ingredient_tags: [],
-      equipment: [],
     }
-  },
-  computed: {
-    recipeList: function() {
-      return JSON.parse(sessionStorage.getItem('recipeList'));
-    },
-    haveData: function() {
-      return sessionStorage.getItem('adminData');
-    },
   },
   beforeUnmount: function() {
     sessionStorage.removeItem('adminData');
     sessionStorage.removeItem('token');
   },
   methods: {
-    updateIngredientTag: async function(id, name) {
-
-      let formData = new FormData();
-      formData.append('name', name);
-
-      this.adminLoading = true;
-      await this.$axios.patch(
-        `${this.$backend}/api/${this.$apiVersion}/admin/ingredient_tags/${id}`, 
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          }
-        },
-      ).then( res => {
-        this.adminLoading = false;
-        if (res.status == 200) {
-          this.adminMsg = "Success";
-        } else {
-          this.adminMsg = "Failed";
-        }
-
-      });
-    },
-    downloadData: async function() {
-      this.isLoading = true;
-      await this.$axios.get(
-        `${this.$backend}/api/${this.$apiVersion}/admin/recipes`,
-        {
-          headers: {
-            authorization: sessionStorage.getItem('token'),
-          }
-        },
-      ).then( res => {
-        if (res.status == 200) {
-          sessionStorage.setItem('adminData', JSON.stringify(res.data));
-          this.isLoading = false;
-        }
-      });
-    },
-    allRecipes: async function() {
-      this.currentComponent="recipeList";
-      await this.$axios.get(
-        `${this.$backend}/api/${this.$apiVersion}/admin/recipes`,
-        {
-          headers: {
-            authorization: sessionStorage.getItem('token'),
-          }
-        },
-      ).then( res => {
-        if (res.status == 200) {
-          sessionStorage.setItem('recipeList', JSON.stringify(res.data));
-        }
-      });
-      //this.currentData=JSON.parse(sessionStorage.getItem('adminData')).recipes;
-    },
-    newRecipe: function() {
-      this.currentComponent="newRecipe";
-    },
-    allIngredients: function() {
-      this.currentComponent="ingredientList";
-      this.currentData=JSON.parse(sessionStorage.getItem('adminData')).ingredients;
-    },
-    allIngredientTags: function() {
-      this.currentComponent="ingredientTagList";
-      this.currentData=JSON.parse(sessionStorage.getItem('adminData')).ingredient_tags;
-    },
-    allEquipment: function() {
-      this.currentComponent="equipmentList";
-      this.currentData=JSON.parse(sessionStorage.getItem('adminData')).equipment;
-    },
-    getRecipe: function(id) {
-      this.currentComponent="singleRecipe";
-      const recipes = JSON.parse(sessionStorage.getItem('adminData')).recipes
-      this.currentData=recipes[id];
-    },
     login: async function() {
       let formData = new FormData();
       formData.append('user', this.username);
@@ -255,6 +106,9 @@ export default {
       if (res.status == 200) {
         this.isAuth = true;
       }
+    }).catch(err => {
+      console.log(err)
+      this.isAuth = false;
     });
   },
 }
